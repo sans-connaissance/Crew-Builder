bf78daf9c0d0f4ba4b5e520523f39c1031774ab8
sans-connaissance <davemalicke@gmail.com>
2022-06-20 15:22:26 -0400

    ADD User Info VCs and placeholder assets

---
+++ Crew Builder/Controllers/User Info/CBUserInfoHeaderVC.swift
@@ -0,0 +1,29 @@
+//
+//  CBUserInfoHeaderVC.swift
+//  Crew Builder
+//
+//  Created by David Malicke on 6/20/22.
+//
+
+import UIKit
+
+class CBUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Crew Builder/Controllers/User Info/ProfileItemVC.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileItemVC.swift
+//  Crew Builder
+//
+//  Created by David Malicke on 6/20/22.
+//
+
+import UIKit
+
+class ProfileItemVC: CBItemForInfoVC {
+    
+}
