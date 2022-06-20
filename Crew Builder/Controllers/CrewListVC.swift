02e434e2338efedfdc00f42052a84a35b80687d5
sans-connaissance <davemalicke@gmail.com>
2022-06-20 16:01:03 -0400

    REGISTER CrewListCell with CrewListVC

--- Crew Builder/Controllers/CrewListVC.swift
+++ Crew Builder/Controllers/CrewListVC.swift
@@ -9,11 +9,93 @@ import UIKit
 
 class CrewListVC: UIViewController {
 
+    
+    let tableView = UITableView()
+    var favorites: [Person] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
-
-        // Do any additional setup after loading the view.
+        configureViewController()
+        configureTableView()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(CrewListCell.self, forCellReuseIdentifier: CrewListCell.reuseID)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Favorites"
+    }
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let favorites):
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower Screen", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                
+            case .failure(let error):
+                self.presentCBAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
+    }
 }
+
+extension CrewListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CrewListCell.reuseID) as! CrewListCell
+        let favorite = favorites[indexPath.row]
+        cell.set(newMember: favorite)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destinationViewController = PersonListVC()
+        destinationViewController.username = favorite.login
+        destinationViewController.title = favorite.login
+        
+        navigationController?.pushViewController(destinationViewController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else {return}
+            guard let error = error else {return}
+            
+            self.presentCBAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "OK")
+        }
+    }
+}
+
--- Crew Builder/Views/Cells/CrewListCell.swift
+++ Crew Builder/Views/Cells/CrewListCell.swift
@@ -9,15 +9,42 @@ import UIKit
 
 class CrewListCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let reuseID = "CrewListCell"
+    let avatarImageView = CBAvatarImageView(frame: .zero)
+    let usernameLabel = CBTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    func set(newMember: Person) {
+        usernameLabel.text = newMember.login
+        avatarImageView.downloadImage(from: newMember.avatarUrl)
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
 }
